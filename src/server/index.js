aa52960a3fe2b7d578298ce2cd5ab188e2febabc
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-06-15 11:03:19 -0300

    port config

--- src/server/index.js
+++ src/server/index.js
@@ -2,22 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const routes = require("../routes/index")
+const routes = require("../routes/index");
 
 const app = express();
 dotenv.config();
 app.use(cors());
-const port = 4000;
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(resp => {
-    console.log("Connected")
-}).catch(error => {
-    console.log("error ", error)
-})
+    useUnifiedTopology: true,
+  })
+  .then((resp) => {
+    console.log("Connected");
+  })
+  .catch((error) => {
+    console.log("error ", error);
+  });
 
 app.use(express.json());
-app.use(routes)
-app.listen(port);
+app.use(routes);
+app.listen(process.env.PORT || 4000);
