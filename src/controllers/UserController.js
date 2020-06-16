be050cbd839ce413d7eda90c556ace43fda61a0e
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-06-16 12:00:18 -0300

    change user controller and model

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,57 +2,59 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-    async showAll(req, res) {
-        let users = await User.find({})
-        return res.status(200).json(users);        
-    },
+  async showAll(req, res) {
+    let users = await User.find({});
+    return res.status(200).json(users);
+  },
 
-    async show(req, res) {
-        const { id } = req.headers;
-        let user = await User.findById(id);
-        if(user) {
-            return res.status(200).json({ user: user})
-        } else {
-            return res.status(400).json({message:"This user doesnt exist!"})
-        }
-    },
-
-    async store(req, res) {
-        const { email, password, name, phone } = req.body;
-
-        if(name.length < 8) {
-            return res.status(400).json({message: "Name must have more then 8 caracters"})
-        }
-
-        let findEmail = await User.findOne({ email });
-
-        if(findEmail) {
-            return res.status(400).json({
-                message: "User already exist with this email"
-            })
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const user = await User.create({
-            name: name,
-            email: email,
-            password: hashedPassword,
-            phone: phone
-        })
-
-        return res.status(200).json({message: "User created!"});
-    },
-
-    async delete(req, res) {
-        const { id } = req.headers;
-        let userDelete = await User.findByIdAndDelete(id);
-
-        if(userDelete) {
-            return res.status(200).json({message: "User deleted"})
-        } else {
-            return res.status(400).json({message: "This user don't exist"})
-        }
+  async show(req, res) {
+    const { _id } = req.headers;
+    let user = await User.findById(_id);
+    if (user) {
+      return res.status(200).json({ user: user });
+    } else {
+      return res.status(400).json({ message: "This user doesnt exist!" });
     }
-}
+  },
+
+  async store(req, res) {
+    const { email, password, name, phone } = req.body;
+
+    if (name.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Name must have more then 8 caracters" });
+    }
+
+    let findEmail = await User.findOne({ email });
+
+    if (findEmail) {
+      return res.status(400).json({
+        message: "User already exist with this email",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+      phone: phone,
+    });
+
+    return res.status(200).json({ message: "User created!" });
+  },
+
+  async delete(req, res) {
+    const { id } = req.headers;
+    let userDelete = await User.findByIdAndDelete(id);
+
+    if (userDelete) {
+      return res.status(200).json({ message: "User deleted" });
+    } else {
+      return res.status(400).json({ message: "This user don't exist" });
+    }
+  },
+};
--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,27 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 let userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 250,
-        minlength: 8
-    },
-    email: {
-        type: String,
-        required: true,
-        maxlength: 200
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: false,
-        minlength: 11
-    }
-})
+  name: {
+    type: String,
+    required: [true, "Name required"],
+    maxlength: 250,
+    minlength: [8, "Name must have at least 8 digits"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email required"],
+    maxlength: 200,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must hava at least 6 digits"],
+  },
+  phone: {
+    type: Number,
+    required: false,
+    minlength: [11, "Phone must have at least 8 digits"],
+  },
+});
 
 module.exports = mongoose.model("User", userSchema);
